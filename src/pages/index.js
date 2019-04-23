4f5242e5294fc07edcc7c5ad4c599186d6e13f00
unknown <dmt5dh@virginia.edu>
2019-04-23 19:11:18 -0400

    Update profile image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { withRouteData } from 'react-static'
 
-import '../../public/images/doge.png'
-//
-
 export default withRouteData(({headline, intro, button, name, snippet}) => (
   <div>
     <div className="jumbotron" style={{textAlign: 'center'}}>
       <div className="row">
         <div className="col-lg-4"></div>
         <div className="col-lg-4">
-          <img src={require('../../public/images/P2090245_headshot.jpg')} alt="my face here" className="rounded-circle profile-img" style={{width:'20em', height:'20em'}}/>
+          <img src={require('../../public/images/profile_pic.jpeg')} alt="my face here" className="rounded-circle profile-img" style={{width:'20em', height:'20em'}}/>
         </div>
         <div className="col-lg-4"></div>
       </div>
